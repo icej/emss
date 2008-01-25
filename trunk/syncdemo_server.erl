5a8cbc18f8064f47ede86924e65ff848fef4aac8
icejmx <icejmx@9828cd77-7344-0410-a166-27c59927caa4>
2008-01-25 13:57:58 +0000

    modify server out client msg

--- trunk/syncdemo_server.erl
+++ trunk/syncdemo_server.erl
@@ -1,2 +1,14 @@
 -module(syncdemo_server).
 -compile(export_all).
+start()->
+	{ok, LSock} = gen_tcp:listen(7000, [binary, {packet, 0},{active, false}]),
+	{ok, Sock} = gen_tcp:accept(LSock),
+	{ok, B}=gen_tcp:recv(Sock, 0),
+	io:format([B]),
+	 ok = gen_tcp:close(Sock).
+
+
+
+    
+    
+      
