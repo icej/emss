d6c1906c68a6c307489915a6972246086d43c764
icejmx <icejmx@9828cd77-7344-0410-a166-27c59927caa4>
2008-01-26 14:20:33 +0000

    modify server,client. add replay but error.

--- trunk/syncdemo_client.erl
+++ trunk/syncdemo_client.erl
@@ -2,5 +2,22 @@
 -compile(export_all).
 client() ->
 {ok, Sock} = gen_tcp:connect("127.0.0.1", 7000, [binary, {packet, 0}]),
-    ok = gen_tcp:send(Sock, "abc\r\n").
-    %%ok = gen_tcp:close(Sock).
+    ok = gen_tcp:send(Sock, "abc\r\n"),
+    A = spawn_link(?MODULE, loop, [Sock,self()]),
+    receive
+        {'new client', Spid} ->
+	    io:format("new client ~w",[Spid]),
+            erlang:monitor(process,Spid)
+    end.
+
+loop(Sock,SPid)->
+    receive
+	{tcp,Sock,Bin} ->
+		io:format("Client received binary = ~p~n" ,[Bin]),
+		Val = binary_to_list(Bin),
+		io:format("Client result = ~p~n" ,[Val]),
+		SPid ! {'new client', self()},
+		ok = gen_tcp:send(Sock, "abc\r\n")
+	after 0 -> loop(Sock,SPid)
+    end.
+    %%ok = gen_tcp:close(Sock).	
--- trunk/syncdemo_server.erl
+++ trunk/syncdemo_server.erl
@@ -11,7 +11,10 @@ loop(Chat=#chat{accepter=A,clients=Cs}) ->
     receive
         {'new client', Client} ->
 	    io:format("new client ~w",[Client#client.pid]),
-            erlang:monitor(process,Client#client.pid)
+            erlang:monitor(process,Client#client.pid);
+	{message,Client,Msg}->
+	    broadcast(Cs,["<~s> ~s\r\n", Client#client.name, Msg]),
+            loop(Chat)
     end.
 accepter(Socket, ServerPid)->
 	{ok, Client} = gen_tcp:accept(Socket),
@@ -20,23 +23,36 @@ accepter(Socket, ServerPid)->
 		refresh -> ?MODULE:accepter(Socket, ServerPid)
 	after 0 -> accepter(Socket, ServerPid)%%不断刷新检测新client加入
 	end.
-
+broadcast(Clients, [Fmt|Args]) ->
+    S = lists:flatten(io_lib:fwrite(Fmt,Args)),
+    lists:foreach(fun (#client{socket=Sock}) ->
+                          gen_tcp:send(Sock,S)
+                  end, Clients).
 
 client(Sock, Server) ->
     gen_tcp:send(Sock, "Please respond with a sensible name.\r\n"),
     {ok,N} = gen_tcp:recv(Sock,0),
     M=binary_to_list(N),
-    case string:tokens(M,"\n") of
+    case string:tokens(M,"\r\n") of
         [Name] ->
 	    Client = #client{socket=Sock, name=Name, pid=self()},
             io:format("client name ~w",[Name]),
-            Server ! {'new client', Client};
+            Server ! {'new client', Client},
+	    client_loop(Client, Server);
             
         _ ->
             gen_tcp:send(Sock, "That wasn't sensible, sorry."),
             gen_tcp:close(Sock)
     end.
 
+client_loop(Client, Server) ->
+    {ok,Recv} = gen_tcp:recv(Client#client.socket,0),
+    RecvAsc = binary_to_list(Recv),
+    lists:foreach(fun(S) -> Server!{message,Client,S} end,string:tokens(RecvAsc,"\r\n")),
+    receive
+        refresh -> ?MODULE:client_loop(Client, Server)
+    after 0 -> client_loop(Client, Server)
+    end.
     
     
       
