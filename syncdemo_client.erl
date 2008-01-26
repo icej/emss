2b068d83ab5bd40a437a653f68e45c0de4d33bff
icejmx <icejmx@9828cd77-7344-0410-a166-27c59927caa4>
2008-01-26 03:41:54 +0000

    sync demo client

---
+++ syncdemo_client.erl
@@ -0,0 +1,7 @@
+-module(syncdemo_client).
+-compile(export_all).
+client() ->
+{ok, Sock} = gen_tcp:connect("127.0.0.1", 7000, [binary, {packet, 0}]),
+    ok = gen_tcp:send(Sock, "abc\n\0"),
+    ok = gen_tcp:send(Sock, "edf\n\0"),
+    ok = gen_tcp:close(Sock).
