147f631a3b63c287dbba8a27e9fa61e84c4ec9a4
icejmx <icejmx@9828cd77-7344-0410-a166-27c59927caa4>
2008-01-28 06:40:00 +0000

    modify server and client.It can echo forevery.

--- syncdemo_client.erl
+++ syncdemo_client.erl
@@ -1,23 +1,15 @@
 -module(syncdemo_client).
 -compile(export_all).
 client() ->
-{ok, Sock} = gen_tcp:connect("127.0.0.1", 7000, [binary, {packet, 0}]),
-    ok = gen_tcp:send(Sock, "abc\r\n"),
-    A = spawn_link(?MODULE, loop, [Sock,self()]),
-    receive
-        {'new client', Spid} ->
-	    io:format("new client ~w",[Spid]),
-            erlang:monitor(process,Spid)
-    end.
+{ok, Sock} = gen_tcp:connect("127.0.0.1", 7000, [{packet, 0},{active, false}]),
+gen_tcp:send(Sock, "abc\r\n"),
+client_recv(Sock).
 
-loop(Sock,SPid)->
-    receive
-	{tcp,Sock,Bin} ->
-		io:format("Client received binary = ~p~n" ,[Bin]),
-		Val = binary_to_list(Bin),
-		io:format("Client result = ~p~n" ,[Val]),
-		SPid ! {'new client', self()},
-		ok = gen_tcp:send(Sock, "abc\r\n")
-	after 0 -> loop(Sock,SPid)
-    end.
-    %%ok = gen_tcp:close(Sock).	
+client_recv(Sock)->
+	case gen_tcp:recv(Sock,0) of
+		{ok, Data} ->
+			gen_tcp:send(Sock, "client love server\r\n"),
+			io:format("client recv: ~w",[Data])
+	end,
+	client_recv(Sock).
+   
--- syncdemo_server.erl
+++ syncdemo_server.erl
@@ -1,58 +1,32 @@
 -module(syncdemo_server).
 -compile(export_all).
--record(chat,{socket,accepter,clients}).
--record(client,{socket,name,pid}).
 
-start()->
-	{ok, Socket} = gen_tcp:listen(7000, [binary, {packet, 0},{active, false}]),
-	 A = spawn_link(?MODULE, accepter, [Socket, self()]),%%为监听client加入起一个进程
-	 loop(#chat{socket=Socket, accepter=A, clients=[]}).
-loop(Chat=#chat{accepter=A,clients=Cs}) ->
-    receive
-        {'new client', Client} ->
-	    io:format("new client ~w",[Client#client.pid]),
-            erlang:monitor(process,Client#client.pid);
-	{message,Client,Msg}->
-	    broadcast(Cs,["<~s> ~s\r\n", Client#client.name, Msg]),
-            loop(Chat)
-    end.
-accepter(Socket, ServerPid)->
-	{ok, Client} = gen_tcp:accept(Socket),
-	spawn(?MODULE, client, [Client, ServerPid]),
-	receive
-		refresh -> ?MODULE:accepter(Socket, ServerPid)
-	after 0 -> accepter(Socket, ServerPid)%%不断刷新检测新client加入
-	end.
-broadcast(Clients, [Fmt|Args]) ->
-    S = lists:flatten(io_lib:fwrite(Fmt,Args)),
-    lists:foreach(fun (#client{socket=Sock}) ->
-                          gen_tcp:send(Sock,S)
-                  end, Clients).
+-define(TCP_OPTIONS,[list, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
-client(Sock, Server) ->
-    gen_tcp:send(Sock, "Please respond with a sensible name.\r\n"),
-    {ok,N} = gen_tcp:recv(Sock,0),
-    M=binary_to_list(N),
-    case string:tokens(M,"\r\n") of
-        [Name] ->
-	    Client = #client{socket=Sock, name=Name, pid=self()},
-            io:format("client name ~w",[Name]),
-            Server ! {'new client', Client},
-	    client_loop(Client, Server);
-            
-        _ ->
-            gen_tcp:send(Sock, "That wasn't sensible, sorry."),
-            gen_tcp:close(Sock)
-    end.
+%% Listen on the given port, accept the first incoming connection and
+%% launch the echo loop on it.
 
-client_loop(Client, Server) ->
-    {ok,Recv} = gen_tcp:recv(Client#client.socket,0),
-    RecvAsc = binary_to_list(Recv),
-    lists:foreach(fun(S) -> Server!{message,Client,S} end,string:tokens(RecvAsc,"\r\n")),
-    receive
-        refresh -> ?MODULE:client_loop(Client, Server)
-    after 0 -> client_loop(Client, Server)
+listen() ->
+    {ok, LSocket} = gen_tcp:listen(7000, ?TCP_OPTIONS),
+    do_accept(LSocket).
+
+%% The accept gets its own function so we can loop easily.  Yay tail
+%% recursion!
+
+do_accept(LSocket) ->
+    {ok, Socket} = gen_tcp:accept(LSocket),
+    spawn(fun() -> do_echo(Socket) end),
+    do_accept(LSocket).
+
+%% Sit in a loop, echoing everything that comes in on the socket.
+%% Exits cleanly on client disconnect.
+
+do_echo(Socket) ->
+    case gen_tcp:recv(Socket, 0) of
+        {ok, Data} ->
+	    io:format("client send: ~w",[Data]),
+            gen_tcp:send(Socket, Data),
+            do_echo(Socket);
+        {error, closed} ->
+            ok
     end.
-    
-    
-      
