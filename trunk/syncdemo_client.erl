4233ef2374dcba6a856489db2cf07ea3adbf0147
icejmx <icejmx@9828cd77-7344-0410-a166-27c59927caa4>
2008-01-29 03:16:13 +0000

    modify server add gen_tcp:controller

--- trunk/syncdemo_client.erl
+++ trunk/syncdemo_client.erl
@@ -1,15 +1,15 @@
 -module(syncdemo_client).
 -compile(export_all).
 client(Name) ->
-{ok, Sock} = gen_tcp:connect("127.0.0.1", 7000, [{packet, 0},{active, false}]),
+{ok, Sock} = gen_tcp:connect("127.0.0.1", 7000, [{packet, 0},{active, true}]),
 gen_tcp:send(Sock, lists:append(["Hello, Come from ", Name])),
 client_recv(Sock,Name).
 
 client_recv(Sock,Name)->
-	case gen_tcp:recv(Sock,0) of
-		{ok, Data} ->
+	receive
+        {tcp, Sock, Bin} ->
 			gen_tcp:send(Sock, lists:append(["client love server ", Name,"\r\n"])),
-			io:format("client recv: ~s",[Data])
+			io:format("client recv: ~s",[Bin])
 	end,
 	client_recv(Sock,Name).
    
