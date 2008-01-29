9f43a882c5ef55b23175509a5d2f9222d0515739
icejmx <icejmx@9828cd77-7344-0410-a166-27c59927caa4>
2008-01-29 06:32:40 +0000

    modify add process.

--- trunk/syncdemo_server.erl
+++ trunk/syncdemo_server.erl
@@ -8,18 +8,19 @@
 
 listen() ->
     {ok, LSocket} = gen_tcp:listen(7000, ?TCP_OPTIONS),
-    register(server,self()),
-    register(accept, spawn(do_accept(LSocket))),
-    do_echo().
+    register(start,self()),
+    %%register(server,spawn(fun() -> show() end)),
+    register(accept, spawn(fun() -> do_accept(LSocket) end)).
 
 %% The accept gets its own function so we can loop easily.  Yay tail
 %% recursion!
-do_echo()->
+show()->
 	receive
 		{recvmsg, Data} ->
 			io:format("client send: ~s\r\n",[Data])
-	end,
-	do_echo().
+	after 0->show()
+	end.
+	
 
 do_accept(LSocket) ->
     {ok, Socket} = gen_tcp:accept(LSocket),
@@ -34,8 +35,8 @@ do_accept(LSocket) ->
 do_echo(Socket) ->
     receive
         {tcp, Socket, Bin} ->
-	    server!{recvmsg,Bin},
-	    %%io:format("client send: ~s\r\n",[Bin]),
+	   %% server!{recvmsg,Bin},
+	    io:format("do_echo: ~s\r\n",[Bin]),
             gen_tcp:send(Socket, Bin),
            do_echo(Socket);
         {tcp_closed, Socket} ->
