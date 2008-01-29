a6e1e4db45a3c3e992822adccf5ad24e60234186
icejmx <icejmx@9828cd77-7344-0410-a166-27c59927caa4>
2008-01-29 15:35:02 +0000

    server modify is non blocking.

--- syncdemo_server.erl
+++ syncdemo_server.erl
@@ -8,18 +8,16 @@
 
 listen() ->
     {ok, LSocket} = gen_tcp:listen(7000, ?TCP_OPTIONS),
-    register(start,self()),
-    %%register(server,spawn(fun() -> show() end)),
+    register(server,spawn(fun() -> show() end)),
     register(accept, spawn(fun() -> do_accept(LSocket) end)).
 
-%% The accept gets its own function so we can loop easily.  Yay tail
-%% recursion!
 show()->
 	receive
 		{recvmsg, Data} ->
-			io:format("client send: ~s\r\n",[Data])
-	after 0->show()
-	end.
+			io:format("client send: ~s\r\n",[Data]),
+			show()
+			
+	end.	
 	
 
 do_accept(LSocket) ->
@@ -35,7 +33,7 @@ do_accept(LSocket) ->
 do_echo(Socket) ->
     receive
         {tcp, Socket, Bin} ->
-	   %% server!{recvmsg,Bin},
+	    server!{recvmsg,Bin},
 	    io:format("do_echo: ~s\r\n",[Bin]),
             gen_tcp:send(Socket, Bin),
            do_echo(Socket);
