5a5d30cc49e21d297a5fa3c23ddd3358ab47823e
icejmx <icejmx@9828cd77-7344-0410-a166-27c59927caa4>
2008-02-08 13:31:26 +0000

    server group can add client

--- client.erl
+++ client.erl
@@ -1,20 +1,25 @@
 -module(client).
 -export([client/2]).
--define(TCP_OPTIONS,[ {packet, 0}, {active, true}, {reuseaddr, true}]).
+-define(TCP_OPTIONS,[binary,{packet, 0}, {active, true}, {reuseaddr, true}]).
 -define(PORT,7000).
 
 client(Name,JoinName) ->
 	{ok, Sock} = gen_tcp:connect("127.0.0.1", ?PORT, ?TCP_OPTIONS),
 	JoinMsg = {join,JoinName,Name},
 	%%io:format("client send: ~w",JoinMsg),
-	gen_tcp:send(Sock, term_to_binary(JoinMsg)).
-	%client_recv(Sock,Name).
+	gen_tcp:send(Sock, term_to_binary(JoinMsg)),
+	client_recv(Sock,JoinName).
 
-client_recv(Sock,Name)->
+client_recv(Sock,JoinName)->
+	SendMsg = {send,JoinName,"I love erlang"},
+	io:format("client send: ~w\r\n",[SendMsg]),
+	gen_tcp:send(Sock, term_to_binary(SendMsg)),
 	receive
         {tcp, Sock, Bin} ->
 			%%gen_tcp:send(Sock, lists:append(["client love server ", Name,"\r\n"])),
-			io:format("client recv: ~s",[Bin])
-	end,
-	client_recv(Sock,Name).
+			{F,Msg} = binary_to_term(Bin),
+			io:format("client recv: ~s ~s\r\n",[F,Msg]),
+			client_recv(Sock,JoinName)
+			%%client_recv(Sock,JoinName)
+	end.
    
--- emssserver.erl
+++ emssserver.erl
@@ -10,6 +10,7 @@
 -define(DEBUG(Fmt, Args), no_debug).
 -endif.
 -record(group,{name,gpid}).
+-record(client, {name,pid,socket}).
 
 init()->
 	{ok, Listen} = gen_tcp:listen(?PORT, ?TCP_OPTIONS),
@@ -27,19 +28,19 @@ client_recv(Socket,ClientName,GroupPid)->
 		{tcp, Socket, Bin} ->
 			?DEBUG("Server recv Data\r\n",""),
 			case binary_to_term(Bin) of
-				{join,JoinName,ClientNameTmp}->
+				{join,JoinName,ClientNameTmp}->%%client join
 					?DEBUG("client_recv Join:~s ~s\r\n",[JoinName,ClientNameTmp]),
 					G=#group{name=JoinName},
 					group_manage ! {join,G,self()},%%get group pid
 						receive
 							{grouppid,GPid}->%%recv group pid
 								?DEBUG("Client_recv grouppid ~w ~s\r\n",[GPid,ClientNameTmp]),
-								GPid!{join,Socket,ClientNameTmp} %% send "join" msg to group
+								GPid!{join,Socket,ClientNameTmp,self()} %% send "join" msg to group
 						end,
 					client_recv(Socket,ClientNameTmp,GPid);
-				{send,Group,Msg}->
-					?DEBUG("client_recv Send:~s ~s\r\n",[Group,Msg]),
-					Group!{ClientName,Msg},
+				{send,GroupName,Msg}->%%client send msg to group,but single group
+					?DEBUG("client_recv Send:~s ~s\r\n",[GroupName,Msg]),
+					GroupPid!{send,ClientName,Msg},
 					client_recv(Socket,ClientName,GroupPid)
 			end;
 		{tcp_closed, Socket} ->
@@ -53,12 +54,9 @@ group_manage(Group)->
 			?DEBUG("group_manage Join:~s\r\n",[JoinGroup#group.name]),
 			case lists:keysearch(JoinGroup#group.name, #group.name, Group) of
 				false ->
-					Pid=spawn(fun() -> group() end),
+					Pid=spawn(fun() -> group([]) end),
 					?DEBUG("group_manage group pid~w\r\n",[Pid]),
-					ClientPid!{grouppid,Pid},
-					%%register(JoinGroupp#group.name,Pid),
-					%%ToGroup = JoinGroupp#group.name
-					%%ToGroup?{add,Socket,ClientName},
+					ClientPid!{grouppid,Pid},%%return client_recv group pid
 					JoinGroupp = #group{name=JoinGroup#group.name,gpid=Pid},
 					Gs = [JoinGroupp|Group],
 					?DEBUG("group_manage group:~w\r\n",[Gs]),
@@ -71,11 +69,36 @@ group_manage(Group)->
 
 	end.
 
-group()->
+group(Client)->
 	?DEBUG("group \r\n",[]),
 	receive
-		{join,Socket,ClientName} ->
+		{join,Socket,ClientName,CPid} ->
 			?DEBUG("group ClientName ~s\r\n",[ClientName]),
-			group()
+			?DEBUG("group Client set ~w\r\n",[Client]),
+			case lists:keysearch(ClientName,#client.name,Client) of
+				false->
+					?DEBUG("group new client\r\n",[]),
+					ClientNew = #client{name=ClientName,pid=CPid,socket=Socket},
+					ClientTmp = [ClientNew|Client],
+					group(ClientTmp);
+				{value,SingleClient}->
+					?DEBUG("group client is set ~s\r\n",[SingleClient#client.name]),
+					group(Client)
+			end;
+		{send,ClientName,Msg}->
+			?DEBUG("group recv client send ~s\r\n",[Msg]),
+			lists:foreach(fun(T)->send(ClientName,T,Msg) end,Client),
+			group(Client)
 	end,
 	ok.
+	
+send(FClientName,TClientName,Msg)->
+	if
+		FClientName /= TClientName#client.name ->
+			?DEBUG("send f:~s t:~s ~s\r\n",[FClientName,TClientName#client.name,Msg]),
+			M = {FClientName,Msg},
+			gen_tcp:send(TClientName#client.socket,term_to_binary(M));
+		true->
+			?DEBUG("send from eq to\r\n",[])
+	end.
+	
